a9ed2624e16aa128e9b364ac031e903e485458dc
Felix Wongso <wongso.felix@gmail.com>
2024-12-13 13:15:52 +0700

    Do front-end for RegisterActivity

--- app/src/main/java/com/mobprog/artlymobile/view/RegisterActivity.java
+++ app/src/main/java/com/mobprog/artlymobile/view/RegisterActivity.java
@@ -1,6 +1,10 @@
 package com.mobprog.artlymobile.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +13,48 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.mobprog.artlymobile.R;
+import com.mobprog.artlymobile.controller.UserController;
+import com.mobprog.artlymobile.utils.ControllerResponse;
+import com.mobprog.artlymobile.utils.ErrorToast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText et_username, et_email, et_password;
+    private Button btn_register, btn_login;
+    private UserController userController;
+
+    private void init() {
+        et_username = findViewById(R.id.et_register_username);
+        et_email = findViewById(R.id.et_register_email);
+        et_password = findViewById(R.id.et_register_password);
+
+        btn_register = findViewById(R.id.btn_register);
+        btn_login = findViewById(R.id.btn_goToLogin);
+
+        userController = new UserController();
+    }
+
+    private void setClickListeners() {
+        btn_register.setOnClickListener((v) -> {
+            String username = et_username.getText().toString();
+            String email = et_email.getText().toString();
+            String password = et_password.getText().toString();
+
+            ControllerResponse response = userController.register(username, email, password);
+
+            if(response.isSuccessful()) {
+                Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                ErrorToast.makeToast(this, response.getMessage());
+            }
+        });
+
+        btn_login.setOnClickListener((v) -> {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +66,8 @@ public class RegisterActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        init();
+        setClickListeners();
     }
 }
